99741c8482faa69f9cdfcfb2783f89f76d7d99d2
mavieuwu <mabigaming26@gmail.com>
2021-05-17 02:23:22 +0200

    que lo arreglo mañana ptmre

---
+++ build/resources/main/static/main.js
@@ -0,0 +1,22 @@
+const tasksection = document.querySelector("#tasklist");
+
+//3 funcion que agrega un div con un texto de lo q escribio el usuario
+function addTask(tasks) {
+    tasks.forEach(task =>{
+        const element =
+            `<div class="taskText">
+            <p class="task">${task.inputTask})
+            </div>`;
+        tasksection.insertAdjacentHTML("beforeend", element)
+    } )
+}
+
+//2 funcion que pide al bakcend lo que hemos escrito en el form
+let loadTasks = () => {
+    fetch("/tasks")
+        .then(r => r.json())
+        .then(addTask)
+};
+
+//1 no sé que hace pero se pone :v
+document.addEventListener("DOMContentLoaded", loadTasks)
